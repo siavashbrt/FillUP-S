535304159635665a3ad11c46f6d5440527fdab47
Siavash Baratchi <Siavash.Baratchi@gmail.com>
2017-01-24 22:39:08 -0800

    Enabling mylocation and compass
    Using CLLocationManager
    I might be able to do the same without CLLocationManager

--- FillUP-Skeleton/FillUP-Skeleton/MainViewController.swift
+++ FillUP-Skeleton/FillUP-Skeleton/MainViewController.swift
@@ -8,9 +8,13 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, CLLocationManagerDelegate {
 
+    //  to get and manage user current location
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,17 +29,46 @@ class MainViewController: UIViewController {
     }
     
     override func loadView() {
+        
+
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        let UserCurrentLocationCoordinates = locationManager.location?.coordinate
+        
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: UserCurrentLocationCoordinates!.latitude, longitude: UserCurrentLocationCoordinates!.longitude, zoom: 16.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        
+        // Enabeling my location.
+        mapView.isMyLocationEnabled = true
+
+        // Enabeling location button. This adds mylocation Button to the buttom right corner of the map
+        mapView.settings.myLocationButton = true
+        
+        // Enabeling compass button.
+        mapView.settings.compassButton = true
+
+
         view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: UserCurrentLocationCoordinates!.latitude, longitude: UserCurrentLocationCoordinates!.longitude)
+        marker.title = "Current Location"
+        marker.snippet = ""
         marker.map = mapView
     }
+    
+
 }
